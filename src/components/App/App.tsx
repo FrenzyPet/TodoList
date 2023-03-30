2d4ac607bd8483aaa9d7d79130f9c507c002ea36
Frenzy <manpatbro@gmail.com>
2023-03-30 23:28:14 +0300

    стартовая найстройка проекта, настройка линтера

---
+++ src/components/App/App.tsx
@@ -0,0 +1,12 @@
+import React, { FC } from 'react';
+import './App.css';
+
+const App: FC = () =>  {
+  return (
+    <div className="App">
+      <h1>Пишем todolist на react+redux+typescript</h1>
+    </div>
+  );
+}
+
+export default App;
